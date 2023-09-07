925a4eb581aa710b5c9257c2e1b709dfc5698610
Matt Weiss <weiss1575@gmail.com>
2023-09-07 03:01:17 -0400

    update Program.cs

--- BreweryAPI/Program.cs
+++ BreweryAPI/Program.cs
@@ -1,13 +1,42 @@
+using BreweryAPI.BLL.Interfaces;
+using BreweryAPI.BLL.Mapping;
+using BreweryAPI.BLL.Services;
+using BreweryAPI.DAL;
+using BreweryAPI.DAL.Interfaces;
+using BreweryAPI.DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<IBreweryService, BreweryService>();
+builder.Services.AddScoped<IDiscountService, DiscountService>();
+builder.Services.AddScoped<IWholesalerService, WholesalerService>();
+builder.Services.AddScoped<IBreweryRepository, BreweryRepository>();
+builder.Services.AddScoped<IWholesalerRepository, WholesalerRepository>();
+
+builder.Services.AddDbContext<BreweryAPIContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BreweryAPIContext"));
+});
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<BreweryAPIContext>();
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
