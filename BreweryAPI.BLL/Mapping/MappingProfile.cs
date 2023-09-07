9cbc7c07284181916806c23b3d11136a4185b27e
Matt Weiss <weiss1575@gmail.com>
2023-09-07 02:55:45 -0400

    Add automapper

---
+++ BreweryAPI.BLL/Mapping/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BreweryAPI.BLL.DataTransferObjects.Beer;
+using BreweryAPI.DAL.Entities;
+
+namespace BreweryAPI.BLL.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Beer, BeerDto>();
+            CreateMap<BeerCreateDto, Beer>();
+            CreateMap<BeerUpdateDto, Beer>();
+        }
+    }
+}
