f10a40e53c4b9e2cc1de56cd3697d2929b752c37
furiax <carlmalfliet@proximus.be>
2023-09-08 20:53:39 +0200

    added the context page

---
+++ BreweryAPI.Furiax/BreweryAPI/BreweryContext.cs
@@ -0,0 +1,19 @@
+﻿using BreweryAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BreweryAPI
+{
+	public class BreweryContext : DbContext
+	{
+        public DbSet<BreweryModel> Breweries { get; set; }
+		public DbSet<BeerModel> Beers { get; set; }
+		public DbSet<WholesalerModel> Wholesalers { get; set; }
+		public DbSet<WholesaleBeersModel> WholesaleBeers { get; set; }
+		public DbSet<OrderModel> Orders { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+		optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=BreweryAPI;Trusted_Connection=True; ");
+
+	}
+
+
+}
