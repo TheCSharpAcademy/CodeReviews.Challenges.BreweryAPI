17769479a23a94ea1b0f1336c890d448b0400d9f
furiax <carlmalfliet@proximus.be>
2023-09-11 14:38:46 +0200

    added the GetBeersByBrewer

--- BreweryAPI.Furiax/BreweryAPI/Controllers/BeerController.cs
+++ BreweryAPI.Furiax/BreweryAPI/Controllers/BeerController.cs
@@ -1,124 +1,140 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using BreweryAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using BreweryAPI;
-using BreweryAPI.Models;
 
 namespace BreweryAPI.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BeerController : ControllerBase
-    {
-        private readonly BreweryContext _context;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class BeerController : ControllerBase
+	{
+		private readonly BreweryContext _context;
 
-        public BeerController(BreweryContext context)
-        {
-            _context = context;
-        }
+		public BeerController(BreweryContext context)
+		{
+			_context = context;
+		}
 
-        // GET: api/Beer
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<BeerModel>>> GetBeers()
-        {
-          if (_context.Beers == null)
-          {
-              return NotFound();
-          }
-            return await _context.Beers.ToListAsync();
-        }
+		// GET: api/BeersByBrewer
+		[HttpGet("BeersByBrewer/{brewerId}")]
+		public async Task<ActionResult<IEnumerable<BeerModel>>> GetBeersByBrewer(int brewerId)
+		{
+			if (_context.Beers == null)
+			{
+				return NotFound();
+			}
+			var beersByBrewer = await _context.Beers
+				.Where(beer => beer.BrewerId == brewerId)
+				.ToListAsync();
 
-        // GET: api/Beer/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<BeerModel>> GetBeerModel(int id)
-        {
-          if (_context.Beers == null)
-          {
-              return NotFound();
-          }
-            var beerModel = await _context.Beers.FindAsync(id);
+			if (beersByBrewer == null || beersByBrewer.Count == 0)
+			{
+				return NotFound();
+			}
 
-            if (beerModel == null)
-            {
-                return NotFound();
-            }
+			return beersByBrewer;
+		}
 
-            return beerModel;
-        }
+		// GET: api/Beer
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<BeerModel>>> GetBeers()
+		{
+			if (_context.Beers == null)
+			{
+				return NotFound();
+			}
+			return await _context.Beers.ToListAsync();
+		}
 
-        // PUT: api/Beer/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutBeerModel(int id, BeerModel beerModel)
-        {
-            if (id != beerModel.BeerId)
-            {
-                return BadRequest();
-            }
 
-            _context.Entry(beerModel).State = EntityState.Modified;
 
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!BeerModelExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+		// GET: api/Beer/5
+		[HttpGet("{id}")]
+		public async Task<ActionResult<BeerModel>> GetBeerModel(int id)
+		{
+			if (_context.Beers == null)
+			{
+				return NotFound();
+			}
+			var beerModel = await _context.Beers.FindAsync(id);
 
-            return NoContent();
-        }
+			if (beerModel == null)
+			{
+				return NotFound();
+			}
 
-        // POST: api/Beer
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<BeerModel>> PostBeerModel(BeerModel beerModel)
-        {
-          if (_context.Beers == null)
-          {
-              return Problem("Entity set 'BreweryContext.Beers'  is null.");
-          }
-            _context.Beers.Add(beerModel);
-            await _context.SaveChangesAsync();
+			return beerModel;
+		}
 
-            return CreatedAtAction("GetBeerModel", new { id = beerModel.BeerId }, beerModel);
-        }
+		// PUT: api/Beer/5
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPut("{id}")]
+		public async Task<IActionResult> PutBeerModel(int id, BeerModel beerModel)
+		{
+			if (id != beerModel.BeerId)
+			{
+				return BadRequest();
+			}
 
-        // DELETE: api/Beer/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBeerModel(int id)
-        {
-            if (_context.Beers == null)
-            {
-                return NotFound();
-            }
-            var beerModel = await _context.Beers.FindAsync(id);
-            if (beerModel == null)
-            {
-                return NotFound();
-            }
+			_context.Entry(beerModel).State = EntityState.Modified;
 
-            _context.Beers.Remove(beerModel);
-            await _context.SaveChangesAsync();
+			try
+			{
+				await _context.SaveChangesAsync();
+			}
+			catch (DbUpdateConcurrencyException)
+			{
+				if (!BeerModelExists(id))
+				{
+					return NotFound();
+				}
+				else
+				{
+					throw;
+				}
+			}
 
-            return NoContent();
-        }
+			return NoContent();
+		}
 
-        private bool BeerModelExists(int id)
-        {
-            return (_context.Beers?.Any(e => e.BeerId == id)).GetValueOrDefault();
-        }
-    }
+		// POST: api/Beer
+		// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+		[HttpPost]
+		public async Task<ActionResult<BeerModel>> PostBeerModel(BeerModel beerModel)
+		{
+			if (_context.Beers == null)
+			{
+				return Problem("Entity set 'BreweryContext.Beers'  is null.");
+			}
+			_context.Beers.Add(beerModel);
+			await _context.SaveChangesAsync();
+
+			return CreatedAtAction("GetBeerModel", new { id = beerModel.BeerId }, beerModel);
+		}
+
+		// DELETE: api/Beer/5
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteBeerModel(int id)
+		{
+			if (_context.Beers == null)
+			{
+				return NotFound();
+			}
+			var beerModel = await _context.Beers.FindAsync(id);
+			if (beerModel == null)
+			{
+				return NotFound();
+			}
+
+			_context.Beers.Remove(beerModel);
+			await _context.SaveChangesAsync();
+
+			return NoContent();
+		}
+
+		private bool BeerModelExists(int id)
+		{
+			return (_context.Beers?.Any(e => e.BeerId == id)).GetValueOrDefault();
+		}
+	}
 }
